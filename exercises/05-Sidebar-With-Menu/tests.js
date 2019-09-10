aef85168079b5d06b73e1beeff7e4cd68a791a28
plucodev <paololucanodeveloper@gmail.com>
2019-09-10 11:58:46 -0400

    updated 05

---
+++ exercises/05-Sidebar-With-Menu/tests.js
@@ -0,0 +1,38 @@
+const fs=require("fs");
+const path=require("path");
+const html=fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf8");
+const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
+
+jest.dontMock("fs");
+
+describe("You should not change existing code", function () {
+    beforeEach(() => {
+        //here I import the HTML into the document
+        document.documentElement.innerHTML=html.toString();
+
+        //apply the styles from the stylesheet if needed
+
+    });
+    afterEach(() => {
+        jest.resetModules();
+    });
+    it("You should not change or delete the existing elements in the head tag", function () {
+
+        let meta1=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta c")
+        let meta2=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta n")
+        let Bootstraplink=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">`)
+        let link=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<link")
+        let title=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<title")
+        expect(meta1).not.toBe(-1)
+        expect(meta2).not.toBe(-1)
+        expect(link).not.toBe(-1)
+        expect(Bootstraplink).not.toBe(-1)
+        expect(title).not.toBe(-1)
+        expect(html.toString().indexOf(`<style`)>-1).toBeFalsy();
+    })
+
+    it("The styles.css file should be empty", function () {
+        expect(css.toString()==="").toBeTruthy();
+    });
+
+});
