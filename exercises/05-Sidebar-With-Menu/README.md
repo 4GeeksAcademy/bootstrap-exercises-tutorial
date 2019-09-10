043abf5935429417544a4b1f1da65214f5af5824
plucodev <paololucanodeveloper@gmail.com>
2019-09-10 18:15:35 -0400

    updated and fixed issues 05

--- exercises/05-Sidebar-With-Menu/README.md
+++ exercises/05-Sidebar-With-Menu/README.md
@@ -7,6 +7,8 @@ Replicate this exact design:
 ## 💡Hint:
 Hint: Create a row with two columns, the column on the left will have a nav inside and the column on the right will have a **jumbotron**.
 
+
 Check the docs:
-- https://getbootstrap.com/docs/4.3/components/nav
+- https://getbootstrap.com/docs/4.3/components/navs
 - https://getbootstrap.com/docs/4.3/components/jumbotron
+- https://getbootstrap.com/docs/4.3/utilities/spacing/
--- exercises/05-Sidebar-With-Menu/tests.js
+++ exercises/05-Sidebar-With-Menu/tests.js
@@ -20,7 +20,7 @@ describe("You should not change existing code", function () {
 
         let meta1=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta c")
         let meta2=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<meta n")
-        let Bootstraplink=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">`)
+        let Bootstraplink=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf(`<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">`)
         let link=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<link")
         let title=document.getElementsByTagName('head')[0].innerHTML.toString().indexOf("<title")
         expect(meta1).not.toBe(-1)
@@ -35,4 +35,68 @@ describe("You should not change existing code", function () {
         expect(css.toString()==="").toBeTruthy();
     });
 
+});
+describe('All the rules in the instructions should be applied', function () {
+    beforeEach(() => {
+        //here I import the HTML into the document
+        document.documentElement.innerHTML=html.toString();
+    });
+    afterEach(() => {jest.resetModules();});
+    it('The body should contains a div tag', function () {
+        let bodyContent=document.getElementsByTagName("body")[0].innerHTML
+        // we can read from the source code
+        // console.log(html.toString());
+        expect(bodyContent.toString().indexOf(`<div`)>-1).toBeTruthy();
+    });
+    it('The div tag should contains the class container-fluid', function () {
+
+        // we can read from the source code
+        // console.log(html.toString());
+        const divTag=document.querySelector("div");
+        expect(divTag.classList.contains("container-fluid")).toBeTruthy();
+
+
+    });
+    it('The div tag should contains a row', function () {
+        let divContent=document.getElementsByTagName("div")[0].innerHTML
+        // we can read from the source code
+        // console.log(html.toString());
+        expect(divContent.toString().indexOf(`<div class="row">`)>-1).toBeTruthy();
+        // expect(divContent.toString().indexOf(`<p>I can't believe that bootstap is so easy, now HTML and CSS are a simple but very usefull technology.</p>`)>-1).toBeTruthy();
+    });
+    it('The row should contains 2 columns with 2 and 10 slots', function () {
+        let rowContent=document.querySelector(".row").children
+        // we can read from the source code
+        // console.log(html.toString());
+        console.log("divCont:", rowContent[0].classList[0])
+        expect(rowContent[0].classList[0]).toBe("col-2")
+        expect(rowContent[1].classList[0]).toBe("col-10")
+    });
+    it('The left column should contains a nav with anchor tags with class nav-link', function () {
+        let navContent=document.querySelector(".col-2").querySelector("nav")
+        console.log("nav", navContent[0])
+        expect(navContent.classList.contains("nav")).toBeTruthy();
+        expect(navContent.classList.contains("flex-column")).toBeTruthy();
+
+    });
+    it('The right column should contains a jumbotron with py-2 class', function () {
+        let rightContent=document.querySelector(".col-10").querySelector(".jumbotron")
+        expect(rightContent.classList.contains("jumbotron")).toBeTruthy();
+        expect(rightContent.classList.contains("py-2")).toBeTruthy();
+
+    });
+    it('The jumbotron should contains an H4, a p and a button ', function () {
+        let jumbotron=document.querySelector(".jumbotron").innerHTML
+        console.log("jumbo", jumbotron)
+        expect(jumbotron.toString().indexOf(`<h4>Hello!</h4>`)>-1).toBeTruthy();
+        expect(jumbotron.toString().indexOf(`<p`)>-1).toBeTruthy();
+        expect(jumbotron.toString().indexOf(`<a`)>-1).toBeTruthy();
+    });
+    it('The button in the jumbotron should be blue and medium size', function () {
+        let rightContent=document.querySelector(".col-10").querySelector(".jumbotron").querySelector("a")
+        expect(rightContent.classList.contains("btn")).toBeTruthy();
+        expect(rightContent.classList.contains("btn-primary")).toBeTruthy();
+        expect(rightContent.classList.contains("btn-md")).toBeTruthy();
+
+    });
 });
