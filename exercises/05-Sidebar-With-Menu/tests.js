c6a1e740c91f250ab1f32d658c884ec33bdd9829
plucodev <paololucanodeveloper@gmail.com>
2019-09-12 18:17:45 -0400

    fixed issues 05

--- exercises/05-Sidebar-With-Menu/tests.js
+++ exercises/05-Sidebar-With-Menu/tests.js
@@ -5,7 +5,7 @@ const css=fs.readFileSync(path.resolve(__dirname, "./styles.css"), "utf8");
 
 jest.dontMock("fs");
 
-describe("You should not change existing code", function () {
+describe("All the tests should pass", function () {
     beforeEach(() => {
         //here I import the HTML into the document
         document.documentElement.innerHTML=html.toString();
@@ -36,19 +36,19 @@ describe("You should not change existing code", function () {
     });
 
 });
-describe('All the rules in the instructions should be applied', function () {
+describe('1. All the rules in the instructions should be applied', function () {
     beforeEach(() => {
         //here I import the HTML into the document
         document.documentElement.innerHTML=html.toString();
     });
     afterEach(() => {jest.resetModules();});
-    it('The body should contains a div tag', function () {
+    it('The body should contain a div tag', function () {
         let bodyContent=document.getElementsByTagName("body")[0].innerHTML
         // we can read from the source code
         // console.log(html.toString());
         expect(bodyContent.toString().indexOf(`<div`)>-1).toBeTruthy();
     });
-    it('The div tag should contains the class container-fluid', function () {
+    it('The div tag should contain the class container-fluid', function () {
 
         // we can read from the source code
         // console.log(html.toString());
@@ -57,14 +57,14 @@ describe('All the rules in the instructions should be applied', function () {
 
 
     });
-    it('The div tag should contains a row', function () {
+    it('The div tag should contain a row', function () {
         let divContent=document.getElementsByTagName("div")[0].innerHTML
         // we can read from the source code
         // console.log(html.toString());
         expect(divContent.toString().indexOf(`<div class="row">`)>-1).toBeTruthy();
         // expect(divContent.toString().indexOf(`<p>I can't believe that bootstap is so easy, now HTML and CSS are a simple but very usefull technology.</p>`)>-1).toBeTruthy();
     });
-    it('The row should contains 2 columns with 2 and 10 slots', function () {
+    it('The row should contain 2 columns with 2 and 10 slots', function () {
         let rowContent=document.querySelector(".row").children
         // we can read from the source code
         // console.log(html.toString());
@@ -72,20 +72,20 @@ describe('All the rules in the instructions should be applied', function () {
         expect(rowContent[0].classList[0]).toBe("col-2")
         expect(rowContent[1].classList[0]).toBe("col-10")
     });
-    it('The left column should contains a nav with anchor tags with class nav-link', function () {
+    it('The left column should contain a nav with anchor tags with class nav-link', function () {
         let navContent=document.querySelector(".col-2").querySelector("nav")
         console.log("nav", navContent[0])
         expect(navContent.classList.contains("nav")).toBeTruthy();
         expect(navContent.classList.contains("flex-column")).toBeTruthy();
 
     });
-    it('The right column should contains a jumbotron with py-2 class', function () {
+    it('The right column should contain a jumbotron with py-2 class', function () {
         let rightContent=document.querySelector(".col-10").querySelector(".jumbotron")
         expect(rightContent.classList.contains("jumbotron")).toBeTruthy();
         expect(rightContent.classList.contains("py-2")).toBeTruthy();
 
     });
-    it('The jumbotron should contains an H4, a p and a button ', function () {
+    it('The jumbotron should contain an H4, a p and a button ', function () {
         let jumbotron=document.querySelector(".jumbotron").innerHTML
         console.log("jumbo", jumbotron)
         expect(jumbotron.toString().indexOf(`<h4>Hello!</h4>`)>-1).toBeTruthy();
